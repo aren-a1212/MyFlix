9aa107c25e2b76aa457783a4e81da2f1f6c68630
aren-a1212 <abdelazizahmed1212@gmail.com>
2025-02-17 08:24:26 -0600

    Fixed app configuration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 //mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true, useUnifiedTopology: true });
 const { check, validationResult } = require('express-validator');
 
